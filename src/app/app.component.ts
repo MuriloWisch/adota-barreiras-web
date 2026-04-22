a7a56977eefe3f3331bbb1665cadf27f9449715d
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-04-21 21:02:41 -0300

    Feat: global spinner on spp components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,42 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { AuthService } from './core/auth/services/auth.service';
+import { LoadingService } from './shared/services/loading.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, AsyncPipe, RouterOutlet, MatProgressBarModule, NavbarComponent, FooterComponent],
   template: `
     <app-navbar></app-navbar>
+
+    <mat-progress-bar
+      mode="indeterminate"
+      *ngIf="loading.loading$ | async"
+      class="global-loader">
+    </mat-progress-bar>
+
     <main [style.padding-top]="'64px'">
       <router-outlet></router-outlet>
     </main>
-    <app-footer *ngIf="showFooter"></app-footer>
+
+    <app-footer *ngIf="auth.isLoggedIn()"></app-footer>
   `,
   styles: [`
     main { min-height: calc(100vh - 64px); }
+    .global-loader {
+      position: fixed; top: 64px; left: 0; right: 0;
+      z-index: 999; height: 3px;
+    }
+    ::ng-deep .global-loader .mdc-linear-progress__bar-inner {
+      border-color: #4CAF50 !important;
+    }
   `],
 })
 export class AppComponent {
-  constructor(public auth: AuthService) {}
-  get showFooter() { return this.auth.isLoggedIn(); }
+  constructor(public auth: AuthService, public loading: LoadingService) {}
 }

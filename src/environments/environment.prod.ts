f6a1bbe53d559b8a7ec1853d0fe4d6ed4bdf6190
MuriloWisch <158103499+MuriloWisch@users.noreply.github.com>
2026-05-06 22:56:19 -0300

    Remove: googleMapsApi key from enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: true,
   apiUrl: (window as any)['env']?.['apiUrl'] ?? '',
   wsUrl: (window as any)['env']?.['wsUrl'] ?? '',
-  googleMapsApiKey: (window as any)['env']?.['googleMapsApiKey'] ?? '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081/api',
   wsUrl: 'http://localhost:8081/ws',
-  googleMapsApiKey: 'SUA_CHAVE_AQUI',
 };

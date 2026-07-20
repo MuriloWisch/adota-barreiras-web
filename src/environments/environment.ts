fa1a9e2cf4ef02d9eb375030a3bbaed2e53bc54e
murilo <murilowisch1@gmail.com>
2026-07-20 11:22:42 -0300

    FIx: environments with localhost and IP config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.18.15:8081/api',
-  wsUrl: 'http://192.168.18.15:8081/ws',
+  apiUrl: 'http://localhost:8081/api',
+  wsUrl: 'http://localhost:8081/ws',
+  //iUrl: 'http://192.168.18.15:8081/api',
+  //Url: 'http://192.168.18.15:8081/ws',
 };
